150f308608bbc58c5672de91782583e045b7ffeb
mohitburdak3-arch <mohitburdak3@gmail.com>
2026-07-08 19:24:32 +0530

    Time: 2 ms (36.34%) | Memory: 10 MB (55.28%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isalphanum(char ch){
+        if((ch>='0'&&ch<='9')||(tolower(ch)>='a'&&tolower(ch)<='z')){
+            return true;
+        }
+        return false;
+    }
+
+    bool isPalindrome(string s) {
+        int st=0,ed=s.length()-1;
+        while(st<ed){
+            if(!isalphanum(s[st])){
+                st++;
+                continue;
+            }
+            if(!isalphanum(s[ed])){
+                ed--;
+                continue;
+            }
+            if(tolower(s[st])!=tolower(s[ed])){
+                return false;
+            }
+            st++;
+            ed--;
+
+        }
+        return true;
+        
+    }
+};
